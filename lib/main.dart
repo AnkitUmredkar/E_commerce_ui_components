54fbf9c405126ef52e2baafe81337e4f7c62fd73
Ankit <ankitumredkar77@gmail.com>
2024-04-13 14:36:35 +0530

    13/04/2024

--- lib/main.dart
+++ lib/main.dart
@@ -1,52 +1,104 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
-//>-Container Example Header File-<
-import 'home/component/Task 1.dart';
-import 'home/component/Task 2.dart';
-import 'home/component/Task 3.dart';
-import 'home/component/Task 4.dart';
-import 'home/component/Task 5.dart';
-import 'home/component/Task 6.dart';
-import 'home/component/Task_7.dart';
-import 'home/component/Task 8.dart';
-
-//>-Button Example Header File-<
-import 'home/component_button/task_1.dart';
-import 'home/component_button/task_2.dart';
-import 'home/component_button/task_3.dart';
-import 'home/component_button/task_4.dart';
-import 'home/component_button/task_5.dart';
-import 'home/component_button/task_6.dart';
-import 'home/component_button/task_7.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SafeArea(
-        child: gradientButton(), //-----> Paste Here The Function
+      home: bolt(),
+    );
+  }
+}
+
+class bolt extends StatefulWidget {
+  const bolt({super.key});
+
+  @override
+  State<bolt> createState() => _boltState();
+}
+
+class _boltState extends State<bolt> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Color(0xFF212121),
+        centerTitle: true,
+        toolbarHeight: 80,
+        title: const Text(
+          'SPLITTER',
+          style: TextStyle(
+              letterSpacing: 1,
+              fontSize: 28,
+              color: Colors.white,
+              fontWeight: FontWeight.w500),
+        ),
       ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+              width: double.infinity,
+              decoration: BoxDecoration(color: Colors.orange),
+              child: SingleChildScrollView(
+                physics: BouncingScrollPhysics(),
+                child: Column(
+                  children: List.generate(10, (index) => ColumnBox(index)),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              width: double.infinity,
+              decoration: const BoxDecoration(color: Colors.red),
+              child: SingleChildScrollView(
+                physics: BouncingScrollPhysics(),
+                scrollDirection: Axis.horizontal,
+                child:
+                Row(children: List.generate(10, (index) => RowBox(index))),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+Container ColumnBox(int index) {
+  return Container(
+    margin: EdgeInsets.all(12),
+    height: 90,
+    width: double.infinity,
+    alignment: Alignment.center,
+    decoration: BoxDecoration(color: Colors.amber),
+    child: Text(
+      '${index + 1}',
+      style: TextStyle(fontSize: 28),
     ),
   );
 }
 
-//>-----------------Container Example Ui-----------------<
-// Copy Below Function amd Paste Above to run UI
-//Task - 1 : odlogo()
-//Task - 2 : MissonRW()
-//Task - 3 : MixUp()
-//Task - 4 : Mashal()
-//Task - 5 : LetterCover()
-//Task - 6 : Cube()
-//Task - 7 : OpenedDoors()
-//Task - 8 : SmileEmoji()
-
-//>-----------------Button Example Ui-----------------<
-// Copy Below Function amd Paste Above to run UI
-//Task - 1 : launchbutton()
-//Task - 2 : darkShadowButton()
-//Task - 3 : liteShadowbutton()
-//Task - 4 : gradientButton()
-//Task - 5 : flagOfIndia()
-//Task - 6 : watchFlutter()
-//Task - 7 : gradientButton2()
+Container RowBox(int index) {
+  return Container(
+    margin: const EdgeInsets.all(12),
+    height: double.infinity,
+    width: 90,
+    alignment: Alignment.center,
+    decoration: const BoxDecoration(color: Colors.grey),
+    child: Text(
+      '${index + 1}',
+      style: TextStyle(fontSize: 28),
+    ),
+  );
+}
